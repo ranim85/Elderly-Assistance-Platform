e73fedd21bee98bbf879e76f5251bce045b2642e
Ranim Rtimi <rtimiranim80@gmail.com>
2026-04-05 00:07:55 +0100

    Fix: Update UserMapper to remove password mapping and compile with UserDTO record

--- Backend-Projet-Elderly-Assistance-Platform/src/main/java/tn/beecoders/elderly/mapper/UserMapper.java
+++ Backend-Projet-Elderly-Assistance-Platform/src/main/java/tn/beecoders/elderly/mapper/UserMapper.java
@@ -1,12 +1,10 @@
 package tn.beecoders.elderly.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import tn.beecoders.elderly.domain.User;
 import tn.beecoders.elderly.dto.UserDTO;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    @Mapping(target = "password", ignore = true)
     UserDTO toDto(User user);
 }
